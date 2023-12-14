eecca29f8493f638a03ee61746ebe8e243ab0f46
Abel-Lagonell <97850130+Abel-Lagonell@users.noreply.github.com>
2023-12-14 11:54:53 -0500

    Restarted app with nothing but the basics
    Started following youtube video, but needs modifications as I go along, like an exit button and a minimize button and such. Right now app is technically functional just need cli for closing

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { BrowserWindow, app } from "electron"
+
+require('electron-reloader')(module);
+
+const createWindow = () => {   
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        titleBarStyle: "hiddenInset",
+        frame: false,
+    });
+
+    mainWindow.webContents.openDevTools();
+    mainWindow.loadFile("src/index.html");
+};
+
+//Loads in the main window when electron is ready to start
+app.whenReady().then(createWindow);
